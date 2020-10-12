083d2e05c9fb5a65871c7159086182487622ddeb
Elton Brayner <eltonbrayner@msn.com>
2020-10-12 17:17:16 -0300

    Finish project

--- src/conversor.js
+++ src/conversor.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import "./conversor.css";
+import React, { useState } from 'react';
+import './conversor.css';
 import {
   Jumbotron,
   Button,
@@ -9,89 +9,144 @@ import {
   Spinner,
   Alert,
   Modal,
-} from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDoubleRight } from "@fortawesome/free-solid-svg-icons";
+} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
+import Axios from 'axios';
 
-import ListarMoedas from "./ListarMoedas"
+import ListarMoedas from './ListarMoedas';
 
 function Conversor() {
+  const [valor, setValor] = useState(1);
+  const [moedaDe, setMoedaDe] = useState('BRL');
+  const [moedaPara, setMoedaPara] = useState('USD');
+  const [spinner, setPinner] = useState(false);
+  const [formValidado, setFormValidado] = useState(false);
+  const [exibirModal, setExibirModal] = useState(false);
+  const [resultadoConversao, setResultadoConversao] = useState('');
+  const [exibirMsgErro, setExibirMsgErro] = useState(false);
 
-  const [valor, setValor] = useState(1)
-  const [moedaDe, setMoedaDe] = useState('BRL')
-  const [moedaPara, setMoedaPara] = useState('USD')
-  const [spinner, setPinner] = useState(false)
-  const [formValidado, setFormValidado] = useState(false)
+  const FIXER_URL =
+    'http://data.fixer.io/api/latest?access_key=dfa106e6d00b0cf9028b46fd0033919b';
 
-  function handleValor(event){
-    const {value} = event.target
-    return !isNaN(value) ? setValor(value) : setValor(valor)
+  function handleValor(event) {
+    const { value } = event.target;
+    return !isNaN(value) ? setValor(value) : setValor(valor);
   }
 
-  function handleMoedaDe(event){
-    const {value} = event.target
-    return setMoedaDe(value)
+  function handleMoedaDe(event) {
+    const { value } = event.target;
+    return setMoedaDe(value);
   }
 
-  function handleMoedaPara(event){
-    const {value} = event.target
-    return setMoedaPara(value)
+  function handleMoedaPara(event) {
+    const { value } = event.target;
+    return setMoedaPara(value);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    setFormValidado(true)
-    if(event.currentTarget.checkValidity() === true){
-      alert(process.env.REACT_APP_NOT_SECRET_CODE)
+    setFormValidado(true);
+    setPinner(true);
+    setExibirMsgErro(false);
+    if (event.currentTarget.checkValidity() === true) {
+      return Axios.get(FIXER_URL)
+        .then((res) => {
+          const cotacao = obterCotacao(res.data);
+          if (cotacao) {
+            setResultadoConversao(
+              `${valor} ${moedaDe} = ${cotacao} ${moedaPara}`
+            );
+            setPinner(false);
+            return setExibirModal(true);
+          }
+          return exibirErro();
+        })
+        .catch((e) => exibirErro());
+    } else {
+      return null;
     }
+  }
+
+  function obterCotacao(props) {
+    if (!props || props.success !== true) return false;
+    const cotacaoDe = props.rates[moedaDe];
+    const cotacaoPara = props.rates[moedaPara];
+    const cotacao = (1 / cotacaoDe) * cotacaoPara * valor;
+    return cotacao.toFixed(2);
+  }
+
+  function exibirErro() {
+    setExibirMsgErro(true);
+    setPinner(false);
+  }
+
+  function handleFecharModal() {
+    setValor('1');
+    setMoedaDe('BRL');
+    setMoedaPara('USD');
+    setFormValidado(false);
+    setPinner(false);
+    return setExibirModal(false);
   }
 
   return (
     <Container>
       <h1 className="text-center">Conversor de Moedas</h1>
-      <Alert variant="danger" show={false} className="text-center">
+      <Alert variant="danger" show={exibirMsgErro} className="text-center">
         Erro ao realizar a conversão
       </Alert>
       <Jumbotron>
         <Form onSubmit={handleSubmit} noValidate validated={formValidado}>
           <Form.Row>
             <Col sm="3">
-              <Form.Control placeholder="0" value={valor} onChange={handleValor} required />
+              <Form.Control
+                placeholder="0"
+                value={valor}
+                onChange={handleValor}
+                required
+              />
             </Col>
             <Col sm="3">
-              <Form.Control 
+              <Form.Control
                 as="select"
                 value={moedaDe}
-                onChange={handleMoedaDe}>
+                onChange={handleMoedaDe}
+              >
                 <ListarMoedas />
               </Form.Control>
             </Col>
-            <Col sm="1" className="text-center" style={{ paddingTop: "5px" }}>
+            <Col sm="1" className="text-center" style={{ paddingTop: '5px' }}>
               <FontAwesomeIcon icon={faAngleDoubleRight} />
             </Col>
             <Col sm="3">
-            <Form.Control 
+              <Form.Control
                 as="select"
                 value={moedaPara}
-                onChange={handleMoedaPara}>
+                onChange={handleMoedaPara}
+              >
                 <ListarMoedas />
               </Form.Control>
             </Col>
             <Col sm="2">
               <Button variant="success" type="submit">
-                <span className={spinner ? null : "hidden"}><Spinner animation="border" size="sm" /></span>
-                <span className={spinner ? "hidden" : null}>Converter</span>
+                <span className={spinner ? null : 'hidden'}>
+                  <Spinner animation="border" size="sm" />
+                </span>
+                <span className={spinner ? 'hidden' : null}>Converter</span>
               </Button>
             </Col>
           </Form.Row>
         </Form>
-        <Modal show={false}>
+        <Modal show={exibirModal} onHide={handleFecharModal}>
           <Modal.Header closeButton>
             <Modal.Title>Conversor de Moedas</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Resultado da conversão</Modal.Body>
+          <Modal.Body>{resultadoConversao}</Modal.Body>
           <Modal.Footer>
-            <Button variant="success">Nova Conversão</Button>
+            <Button variant="success" onClick={handleFecharModal}>
+              Nova Conversão
+            </Button>
           </Modal.Footer>
         </Modal>
       </Jumbotron>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Conversor from "./conversor";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Conversor from './conversor';
+import * as serviceWorker from './serviceWorker';
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Conversor />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Conversor />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
